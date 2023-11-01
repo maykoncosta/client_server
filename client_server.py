d939e70e8d1494c2e23767e9eff07b66559c094b
jose <jose.brito@maisunifacisa.com.br>
2023-11-01 00:23:48 -0300

    personalizacao

--- client_server.py
+++ client_server.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+from datetime import datetime
 
 # Inicializando o Firebase
 cred = credentials.Certificate("client-server-52528-cdc8390cfb95.json")
@@ -11,24 +12,40 @@ except ValueError:
 
 db = firestore.client()
 
-# Função para adicionar post ao Firestore
 def add_post(username, message):
     doc_ref = db.collection('posts').document()
     doc_ref.set({
         'username': username,
-        'message': message
+        'message': message,
+        'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     })
 
 # Função para obter posts do Firestore
 def get_posts():
     posts = db.collection('posts').stream()
     for post in posts:
-        st.write(f"{post.to_dict()['username']}: {post.to_dict()['message']}")
+        with st.chat_message("user"):
+            st.write(f"{post.to_dict()['username']} ({post.to_dict()['timestamp']}): :green[{post.to_dict()['message']}]")
+
+def clear_posts():
+    docs = db.collection('posts').stream()
+    for doc in docs:
+        doc.reference.delete()
 
 # Interface do Streamlit
-st.title('Fórum')
+st.title(':violet[Fórum]')
+st.header('Divirta-se no forum', divider='rainbow')
+st.subheader('Projeto válido para :blue[Sistemas Distribuidos] :sunglasses:')
 username = st.text_input('Nome de usuário')
 message = st.text_input('O que você está pensando?')
 if st.button('Postar'):
     add_post(username, message)
+    st.balloons()
+    username = ""
+if st.button('Atualizar'):
+    get_posts()
+    
+# botão para limpar todas as mensagens
+if st.button('Limpar todas as mensagens'):
+    clear_posts()
 get_posts()
